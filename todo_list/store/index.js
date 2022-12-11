ef90cb2c6c267dcc77ba048b411c1ad8f09eefe0
youvakadji <120023771+youvakadji@users.noreply.github.com>
2022-12-11 21:42:26 +0100

    Update index.js

--- todo_list/store/index.js
+++ todo_list/store/index.js
@@ -7,15 +7,20 @@ export const state = () => ({
 export const mutations = {
 	ADD_TASK(state, task) {
 		state.tasks = [{ content: task, done: false }, ...state.tasks];
-		var parsed = JSON.stringify(state.tasks);
+		const parsed = JSON.stringify(state.tasks);
 		localStorage.setItem('tasks', parsed);
+		
 	},
 
 	REMOVE_TASK(state, task) {
 		state.tasks.splice(state.tasks.indexOf(task), 1);
+		const parsed = JSON.stringify(state.tasks);
+		localStorage.setItem('tasks', parsed);
 	},
 
 	TOGGLE_TASK(state, task) {
 		task.done = !task.done;
+		const parsed = JSON.stringify(state.tasks);
+		localStorage.setItem('tasks', parsed);
 	}
 }
