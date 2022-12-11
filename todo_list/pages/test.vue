16edb3d472d1cb8de913744d0578ec1aa17a7730
youvakadji <120023771+youvakadji@users.noreply.github.com>
2022-12-11 21:41:57 +0100

    Update test.vue

---
+++ todo_list/pages/test.vue
@@ -0,0 +1,41 @@
+<template>
+    <main>
+      <h1>Task Board</h1>
+      <p>Create a list of tasks</p>
+  
+      <!-- <button @click="addTask">Add todo-list</button> -->
+  
+      <div class="tasks">
+        <input
+          type="text"
+          v-model="value"
+        />
+      </div>
+    </main>
+  </template>
+  
+  <script>
+  export default {
+    data (){
+      return {
+        value: [],
+      }
+    },
+    
+    watch: {
+      value(newValue){
+          newValue = [{value: newValue}]
+          var parsed = JSON.stringify(newValue)
+          localStorage.setItem('todolist', parsed)
+      },
+    },
+    mounted() {
+      if (localStorage.getItem('todolist')) {
+        var valuejson = JSON.parse(localStorage.getItem('todolist'))
+        this.value = valuejson[0].value
+      }
+    },
+    
+    
+  }
+  </script>
